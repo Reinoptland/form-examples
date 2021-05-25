6d175a74791cf7467ecf555e0d3057fefbc09ec6
Reinoptland <reinoptland@gmail.com>
2021-05-25 18:27:28 +0200

    Todolist for controlled form

--- src/App.js
+++ src/App.js
@@ -5,17 +5,32 @@ import "./App.css";
 // Email -> string
 // Jaren ervaring -> number
 
+// controlled components
+// - state aanmaken voor elk input veld
+// - value doorgeven aan het input veld (getter)
+// - onChange handler voor elk input veld (setter)
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <form className="App-header">
           <label htmlFor="name">Naam</label>
-          <input id="name" name="name" type="text" />
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="your name here ..."
+          />
           <label htmlFor="email">Email</label>
-          <input id="email" name="email" type="email" />
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="you@you.com"
+          />
           <label htmlFor="xp">Years of experience</label>
-          <input id="xp" name="xp" type="number" />
+          <input id="xp" name="xp" type="number" placeholder="0" />
           <input type="submit" />
         </form>
       </header>
